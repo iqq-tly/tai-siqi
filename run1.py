44399bb5911d98e5946576e2810692e6b5f436d5
iqq-tly <camellia09030713@gmail.com>
2024-06-10 16:37:04 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -62,19 +62,13 @@ class PI_DeepONet(nn.Module):
     # Define PDE residual
     def residual_net(self,u1,u2,u3,x,t):
         s=self.operator_net(u1,u2,u3,x,t)
-        
-        if s.device.type == 'cuda':
-            print("s 在 CUDA 设备上")
         s_x =jacrev(self.operator_net,argnums=3)(u1,u2,u3,x,t).sum(dim=0)
-        if s_x.device.type == 'cuda':
-            print("s_x 在 CUDA 设备上")
         s_xx =(hessian(self.operator_net,argnums=3)(u1,u2,u3,x,t).sum(dim=0)).sum(dim=0)
-        if s_xx.device.type == 'cuda':
-            print("s_xx 在 CUDA 设备上")
         s_t =jacrev(self.operator_net,argnums=4)(u1,u2,u3,x,t).sum(dim=0)
-        if s_t.device.type == 'cuda':
-            print("s_t 在 CUDA 设备上")
-        res =s_t-(1/2)*(0.165856529**2)*(x**2)*s_xx-0.0256*x*s_x+0.0256*s
+        member1 = torch.tensor(0.5, device='cuda')
+        member2 = torch.tensor(0.165856529, device='cuda')
+        member3 = torch.tensor(0.165856529, device='cuda')
+        res =s_t-(member1)*(member2**2)*(x**2)*s_xx-member3*x*s_x+member3*s
         return res
         # r =0.025610
         # v=0.165856529
