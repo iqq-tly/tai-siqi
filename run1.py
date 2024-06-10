751e6454c2aa32f248a87bfa72eaf99eff03e6c3
iqq-tly <camellia09030713@gmail.com>
2024-06-10 15:53:08 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -49,7 +49,7 @@ class PI_DeepONet:
         BC3 = self.model3(u3)
         BC2= BC2.to(device)
         BC2= BC2.to(device)
-        B=(BC1*BC2*BC3).to(device)
+        B=BC1*BC2*BC3
         return B
 
 
