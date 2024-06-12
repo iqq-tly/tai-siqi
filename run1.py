62031654285a01f21e6a1502e7fc31a3a9f881b8
iqq-tly <camellia09030713@gmail.com>
2024-06-12 15:17:58 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -164,7 +164,7 @@ class PI_DeepONet(nn.Module):
                     self.optimizer.zero_grad()
                     bc_loss= self.loss_bcs(u1,u2,u3, x_i, t_i,outputs_i)
                     pde_loss=self.loss_res(u1,u2,u3,x_b, t_b, outputs_b)
-                    loss =0.01*pde_loss+0.001*bc_loss
+                    loss =0.01*pde_loss+bc_loss
                     loss.backward()
                     return loss
 
