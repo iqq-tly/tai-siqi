70b316df542098b1c0864069c7d30acf2b11e906
iqq-tly <camellia09030713@gmail.com>
2024-06-12 17:35:46 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -164,7 +164,7 @@ class PI_DeepONet(nn.Module):
                     self.optimizer.zero_grad()
                     bc_loss= self.loss_bcs(u1,u2,u3, x_i, t_i,outputs_i)
                     pde_loss=self.loss_res(u1,u2,u3,x_b, t_b, outputs_b)
-                    loss =pde_loss+500*bc_loss
+                    loss =100*pde_loss+500*bc_loss
                     loss.backward()
                     return loss
 
@@ -345,11 +345,11 @@ dataloader2 = DataLoader(dataset2, batch_size=batch_size2, shuffle=True)
 
 
 
-model1 =KAN([3, 2,2, 1], base_activation=nn.Identity)
-model2 = KAN([3,2,2 ,1], base_activation=nn.Identity)
-model3 = KAN([3,2,2,1], base_activation=nn.Identity)
-model4 = KAN([100,2,2,2], base_activation=nn.Identity)
-model5 = KAN([2,2,2,2], base_activation=nn.Identity)
+model1 =KAN([3, 2, 1], base_activation=nn.Identity)
+model2 = KAN([3,2,1], base_activation=nn.Identity)
+model3 = KAN([3,2,1], base_activation=nn.Identity)
+model4 = KAN([100,2,2], base_activation=nn.Identity)
+model5 = KAN([2,2,2], base_activation=nn.Identity)
 model= PI_DeepONet(model1,model2,model3,model4,model5)
 model.to(device)
 model.train(u_1,u_2,u_3,dataloader1,dataloader2)
