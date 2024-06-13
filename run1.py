d4d4039211841a5f161ad15706acb1f90056659a
iqq-tly <camellia09030713@gmail.com>
2024-06-13 10:55:06 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -150,10 +150,10 @@ class PI_DeepONet(nn.Module):
         # params = (model1.parameters(), model2.parameters())
         # Initialize optimizer
 
-        self.optimizer = torch.optim.LBFGS(params, lr=0.06,history_size=10, line_search_fn="strong_wolfe",
+        self.optimizer = torch.optim.LBFGS(params, lr=0.03,history_size=10, line_search_fn="strong_wolfe",
                                tolerance_grad=1e-32, tolerance_change=1e-32)
     
-        pbar = tqdm(range(50), desc='description')
+        pbar = tqdm(range(100), desc='description')
        
         for _ in pbar:
            
@@ -164,7 +164,7 @@ class PI_DeepONet(nn.Module):
                     self.optimizer.zero_grad()
                     bc_loss= self.loss_bcs(u1,u2,u3, x_i, t_i,outputs_i)
                     pde_loss=self.loss_res(u1,u2,u3,x_b, t_b, outputs_b)
-                    loss =50*pde_loss+100*bc_loss
+                    loss =pde_loss+100*bc_loss
                     loss.backward()
                     return loss
 
