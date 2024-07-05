41c8d41d55e5681479e75037c0b80570d0350f7f
iqq-tly <camellia09030713@gmail.com>
2024-07-05 11:26:12 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -342,7 +342,7 @@ def generate_one_training_data(key,P,Q,K,M,r,v,T):
 key = random.PRNGKey(0)
 
 K=2.411
-P =600 # number of output sensors, 100 for each side
+P =300 # number of output sensors, 100 for each side
 Q =200  # number of collocation points for each input sample
 M = 5000
 r =0.025610
