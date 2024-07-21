0e01778027012fab9dd95d602551eb9fe3e0cae8
iqq-tly <camellia09030713@gmail.com>
2024-07-21 19:36:45 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -221,7 +221,7 @@ def generate_one_training_data(key,P,Q,K,M,r,v,T):
     N = 512
     gp_params = (1.0,0.2)
     jitter = 1e-10
-    X = np.linspace(xmin, xmax, N)[:,None]
+    X = np.linspace(0,3*K, N)[:,None]
     K = RBF(X, X, gp_params)
     L = np.linalg.cholesky(K + jitter*np.eye(N))
     gp_sample = np.dot(L, random.normal(subkeys[0], (N,)))
