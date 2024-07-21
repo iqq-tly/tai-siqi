06150dc7ac7fe7b0ac2e9cebe275633a2b6a246b
iqq-tly <camellia09030713@gmail.com>
2024-07-21 10:19:18 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -171,9 +171,9 @@ class PI_DeepONet(nn.Module):
                     loss.backward()
                     return loss
 
-            # if _ % 5 == 0 and _ < 50:
-                model1.update_grid_from_samples(u1)
-                model2.update_grid_from_samples(u2)
+            # # if _ % 5 == 0 and _ < 50:
+            #     model1.update_grid_from_samples(u1)
+            #     model2.update_grid_from_samples(u2)
                 # model4.update_grid_from_samples(u_b1)
                 # model5.update_grid_from_samples(u_b2)
                 self.optimizer.step(closure)
