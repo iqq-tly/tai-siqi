7acf7711bf0b51c7eb06114f43469ab2e6aafc5d
iqq-tly <camellia09030713@gmail.com>
2024-07-22 16:35:52 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -168,7 +168,7 @@ class PI_DeepONet(nn.Module):
                     bc_loss= self.loss_bcs(u1,u2,u_s1,u_s2,x_i, t_i,outputs_i)
                     pde_loss=self.loss_res(u1,u2,u_s1,u_s2,x_b,t_b,outputs_b)
                     # _,brunk_net_loss= model.brunk_net(u1, u2,u_s1, u_s2)
-                    loss =1000*pde_loss+1000*bc_loss
+                    loss =pde_loss+bc_loss
                     loss.backward()
                     return loss
 
