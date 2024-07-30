03c281a343a637ab060ba09ce84b0a13bfa64379
iqq-tly <camellia09030713@gmail.com>
2024-07-30 08:46:48 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -152,10 +152,10 @@ class PI_DeepONet(nn.Module):
         # params = (model1.parameters(), model2.parameters())
         # Initialize optimizer
 
-        self.optimizer = torch.optim.LBFGS(params, lr=0.006,history_size=10, line_search_fn="strong_wolfe",
+        self.optimizer = torch.optim.LBFGS(params, lr=0.01,history_size=10, line_search_fn="strong_wolfe",
                                tolerance_grad=1e-64, tolerance_change=1e-64)
     
-        pbar = tqdm(range(10), desc='description')
+        pbar = tqdm(range(30), desc='description')
     
        
         for _ in pbar:
