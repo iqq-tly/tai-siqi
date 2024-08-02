8c018457b8354aac180e02ca6be40f496954853d
iqq-tly <camellia09030713@gmail.com>
2024-08-02 21:04:21 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -156,7 +156,7 @@ class PI_DeepONet(nn.Module):
 
         # self.optimizer = torch.optim.LBFGS(params, lr=0.001,history_size=10, line_search_fn="strong_wolfe",
         #                        tolerance_grad=1e-64, tolerance_change=1e-64)
-        self.optimizer = torch.optim.RMSprop(params, lr=0.005)
+        self.optimizer = torch.optim.Nadam(params, lr=0.005)
         pbar = tqdm(range(100), desc='description')
     
        
